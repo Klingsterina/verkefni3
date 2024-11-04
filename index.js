d9b67c4fb15916d249a8bb986f068f526348a9a5
Klingsterina <eyglo2001@gmail.com>
2024-11-04 14:00:56 +0000

    breytti og hef annann byrjun á verkefni

--- index.js
+++ index.js
@@ -1,47 +1,227 @@
-///////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
 //    Sýnidæmi í Tölvugrafík
-//     Einfaldasta WebGL forritið.  Teiknar einn rauðan þríhyrning.
+//    Byggt á sýnisforriti í C fyrir OpenGL, höfundur óþekktur.
 //
-//    Hjálmtýr Hafsteinsson, ágúst 2024
-///////////////////////////////////////////////////////////////////
+//     Bíll sem keyrir í hringi í umhverfi með húsum.  Hægt að
+//    breyta sjónarhorni áhorfanda með því að slá á 1, 2, ..., 8.
+//    Einnig hægt að breyta hæð áhorfanda með upp/niður örvum.
+//    Leiðrétt útgáfa fyrir réttan snúning í MV.js
+//
+//    Hjálmtýr Hafsteinsson, september 2024
+////////////////////////////////////////////////////////////////////
+var canvas;
 var gl;
-var points;
+
+// position of the track
+var TRACK_RADIUS = 100.0;
+var TRACK_INNER = 90.0;
+var TRACK_OUTER = 110.0;
+var TRACK_PTS = 100;
+
+var BLUE = vec4(0.0, 0.0, 1.0, 1.0);
+var RED = vec4(1.0, 0.0, 0.0, 1.0);
+var GRAY = vec4(0.4, 0.4, 0.4, 1.0);
+
+var numCubeVertices  = 36;
+var numTrackVertices  = 2*TRACK_PTS + 2;
+
+
+// variables for moving car
+var carDirection = 0.0;
+var carXPos = 100.0;
+var carYPos = 0.0;
+var height = 0.0;
+
+// current viewpoint
+var view = 1;
+
+var colorLoc;
+var mvLoc;
+var pLoc;
+var proj;
+
+var cubeBuffer;
+var trackBuffer;
+var vPosition;
+
+// the 36 vertices of the cube
+var cVertices = [
+    // front side:
+    vec3( -0.5,  0.5,  0.5 ), vec3( -0.5, -0.5,  0.5 ), vec3(  0.5, -0.5,  0.5 ),
+    vec3(  0.5, -0.5,  0.5 ), vec3(  0.5,  0.5,  0.5 ), vec3( -0.5,  0.5,  0.5 ),
+    // right side:
+    vec3(  0.5,  0.5,  0.5 ), vec3(  0.5, -0.5,  0.5 ), vec3(  0.5, -0.5, -0.5 ),
+    vec3(  0.5, -0.5, -0.5 ), vec3(  0.5,  0.5, -0.5 ), vec3(  0.5,  0.5,  0.5 ),
+    // bottom side:
+    vec3(  0.5, -0.5,  0.5 ), vec3( -0.5, -0.5,  0.5 ), vec3( -0.5, -0.5, -0.5 ),
+    vec3( -0.5, -0.5, -0.5 ), vec3(  0.5, -0.5, -0.5 ), vec3(  0.5, -0.5,  0.5 ),
+    // top side:
+    vec3(  0.5,  0.5, -0.5 ), vec3( -0.5,  0.5, -0.5 ), vec3( -0.5,  0.5,  0.5 ),
+    vec3( -0.5,  0.5,  0.5 ), vec3(  0.5,  0.5,  0.5 ), vec3(  0.5,  0.5, -0.5 ),
+    // back side:
+    vec3( -0.5, -0.5, -0.5 ), vec3( -0.5,  0.5, -0.5 ), vec3(  0.5,  0.5, -0.5 ),
+    vec3(  0.5,  0.5, -0.5 ), vec3(  0.5, -0.5, -0.5 ), vec3( -0.5, -0.5, -0.5 ),
+    // left side:
+    vec3( -0.5,  0.5, -0.5 ), vec3( -0.5, -0.5, -0.5 ), vec3( -0.5, -0.5,  0.5 ),
+    vec3( -0.5, -0.5,  0.5 ), vec3( -0.5,  0.5,  0.5 ), vec3( -0.5,  0.5, -0.5 )
+];
+
+// vertices of the track
+var tVertices = [];
+
 
 window.onload = function init()
 {
-    var canvas = document.getElementById( "gl-canvas" );
+    canvas = document.getElementById( "gl-canvas" );
     
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
-    var vertices = new Float32Array([-1, -1, 0, 1, 1, -1]);
-
-    //  Configure WebGL
-
     gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 0.95, 1.0, 1.0, 1.0 );
+    gl.clearColor( 0.7, 1.0, 0.7, 1.0 );
     
+    gl.enable(gl.DEPTH_TEST);
+
+    //
     //  Load shaders and initialize attribute buffers
-    
+    //
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
     
-    // Load the data into the GPU
+    createTrack();
     
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW );
+    // VBO for the track
+    trackBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, trackBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(tVertices), gl.STATIC_DRAW );
 
-    // Associate shader variables with our data buffer
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    // VBO for the cube
+    cubeBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cubeBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(cVertices), gl.STATIC_DRAW );
+
+
+    vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
 
+    colorLoc = gl.getUniformLocation( program, "fColor" );
+    
+    mvLoc = gl.getUniformLocation( program, "modelview" );
+
+    // set projection
+    pLoc = gl.getUniformLocation( program, "projection" );
+    proj = perspective( 50.0, 1.0, 1.0, 500.0 );
+    gl.uniformMatrix4fv(pLoc, false, flatten(proj));
+
+    document.getElementById("Viewpoint").innerHTML = "1: Fjarlægt sjónarhorn";
+    document.getElementById("Height").innerHTML = "Viðbótarhæð: "+ height;
+
+    // Event listener for keyboard
+    window.addEventListener("keydown", function(e){
+
+    });
     render();
-};
+}
 
 
-function render() {
-    gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.TRIANGLES, 0, 3 );
+// create the vertices that form the car track
+function createTrack() {
+
+    var theta = 0.0;
+    for( var i=0; i<=TRACK_PTS; i++ ) {
+        var p1 = vec3(TRACK_OUTER*Math.cos(radians(theta)), TRACK_OUTER*Math.sin(radians(theta)), 0.0);
+        var p2 = vec3(TRACK_INNER*Math.cos(radians(theta)), TRACK_INNER*Math.sin(radians(theta)), 0.0) 
+        tVertices.push( p1 );
+        tVertices.push( p2 );
+        theta += 360.0/TRACK_PTS;
+    }
+}
+
+
+// draw a house in location (x, y) of size size
+function house( x, y, size, mv ) {
+
+    gl.uniform4fv( colorLoc, RED );
+    
+    mv = mult( mv, translate( x, y, size/2 ) );
+    mv = mult( mv, scalem( size, size, size ) );
+
+    gl.bindBuffer( gl.ARRAY_BUFFER, cubeBuffer );
+    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
+
+    gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
+    gl.drawArrays( gl.TRIANGLES, 0, numCubeVertices );
+}
+    
+
+// draw the circular track and a few houses (i.e. red cubes)
+function drawScenery( mv ) {
+
+    // draw track
+    gl.uniform4fv( colorLoc, GRAY );
+    gl.bindBuffer( gl.ARRAY_BUFFER, trackBuffer );
+    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
+
+    gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
+    gl.drawArrays( gl.TRIANGLE_STRIP, 0, numTrackVertices );
+
+
+    // draw houses    
+    house(-20.0, 50.0, 5.0, mv);
+    house(0.0, 70.0, 10.0, mv);
+    house(20.0, -10.0, 8.0, mv);
+    house(40.0, 120.0, 10.0, mv);
+    house(-30.0, -50.0, 7.0, mv);
+    house(10.0, -60.0, 10.0, mv);
+    house(-20.0, 75.0, 8.0, mv);
+    house(-40.0, 140.0, 10.0, mv);
+
+}
+
+
+// draw car as two blue cubes
+function drawCar( mv ) {
+
+    // set color to blue
+    gl.uniform4fv( colorLoc, BLUE );
+    
+    gl.bindBuffer( gl.ARRAY_BUFFER, cubeBuffer );
+    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
+
+    var mv1 = mv;
+    // lower body of the car
+    mv = mult( mv, scalem( 10.0, 3.0, 2.0 ) );
+    mv = mult( mv, translate( 0.0, 0.0, 0.5 ) );
+
+    gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
+    gl.drawArrays( gl.TRIANGLES, 0, numCubeVertices );
+
+    // upper part of the car
+    mv1 = mult( mv1, scalem( 4.0, 3.0, 2.0 ) );
+    mv1 = mult( mv1, translate( -0.2, 0.0, 1.5 ) );
+
+    gl.uniformMatrix4fv(mvLoc, false, flatten(mv1));
+    gl.drawArrays( gl.TRIANGLES, 0, numCubeVertices );
+}
+    
+
+function render()
+{
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    carDirection += 3.0;
+    if ( carDirection > 360.0 ) carDirection = 0.0;
+
+    carXPos = TRACK_RADIUS * Math.sin( radians(carDirection) );
+    carYPos = TRACK_RADIUS * Math.cos( radians(carDirection) );
+
+    var mv = mat4();
+        // Distant and stationary viewpoint
+	    mv = lookAt( vec3(250.0, 0.0, 100.0+height), vec3(0.0, 0.0, 0.0), vec3(0.0, 0.0, 1.0) );
+	    drawScenery( mv );
+	    mv = mult( mv, translate( carXPos, carYPos, 0.0 ) );
+	    mv = mult( mv, rotateZ( -carDirection ) ) ;
+	    drawCar( mv );
+    requestAnimFrame( render );
 }
